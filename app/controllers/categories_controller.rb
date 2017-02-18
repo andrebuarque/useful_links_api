8d54328e36723c90cac0696a8f2e2b3d15d7a8f3
André Buarque <andre.buarque90@gmail.com>
2017-02-18 00:17:16 -0300

    crud category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,32 @@ class CategoriesController < ApplicationController
 	def show
 		render :json => Category.find(params[:id])
 	end
+
+	def create
+		begin
+			Category.create(:name => params[:name])
+			render :status => :ok
+		rescue Exception => e
+			render :status => 500
+		end
+	end
+
+	def update
+		begin
+			Category.update(params[:id], params.permit(:name))
+			render :status => :ok
+		rescue Exception => e
+			render :status => 500
+		end
+	end
+
+	def destroy
+		begin
+			Category.find(params[:id]).delete
+			render :status => :ok
+		rescue Exception => e
+			render :status => 500
+		end
+	end
+	
 end
